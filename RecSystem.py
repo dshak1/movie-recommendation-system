c4b5a5e7295060339c044442fff442d3999aae55
dshak1 <dsa158@sfu.ca>
2024-09-09 13:49:13 -0700

    Add files via upload

--- RecSystem.py
+++ RecSystem.py
@@ -1,8 +1,8 @@
 import pandas as pd
+from sklearn.metrics.pairwise import cosine_similarity
 # import zipfile
 # with zipfile.ZipFile('ml-latest-small.zip', 'r') as zip_ref:
 #     zip_ref.extractall('data')
-from sklearn.metrics.pairwise import cosine_similarity
 
 # class user:
 #     # User inputted movies they like which we can use to find something similar
@@ -12,49 +12,83 @@ from sklearn.metrics.pairwise import cosine_similarity
 #     recommendationShortlist = []
 # # print(user.moviePicks[2])
 
+# Load Movies Metadata. Use low_memory false 
+# since we have small dataset and data inference could be wrong
 metadata = pd.read_csv('movies_metadata.csv', low_memory = False)
 
-C = metadata['vote_average'].mean()
+#find average vote count
+vote_average = metadata['vote_average'].mean()
 
+#find the top 10% vote count number 
 m = metadata['vote_count'].quantile(.9)
 
-q_movies = metadata.copy().loc[metadata['vote_count'] >= m]
-
-q_movies.shape
-metadata.shape
-
+# create a new filtered dataset with vote count over 160.0
+top_movies = metadata.copy().loc[metadata['vote_count'] >= m]
 
 
 # Function that computes the weighted rating of each movie
-# where c is the average and m is the minimum num of votes to be considered
-def weighted_rating(x, m=m, C=C):
-    v = x['vote_count']
-    R = x['vote_average']
+def weighted_rating(x, m=m, vote_average=vote_average):
+    v = x['vote_count'] #popularity metric
+    R = x['vote_average'] #rating of a movie
     # Calculation based on the IMDB formula
-    return (v/(v+m) * R) + (m/(m+v) * C)
+    return (v/(v+m) * R) + (m/(m+v) * vote_average)
 
 # Define a new feature 'score' and calculate its value with `weighted_rating()`
-q_movies['score'] = q_movies.apply(weighted_rating, axis=1)
+top_movies['score'] = top_movies.apply(weighted_rating, axis=1) #axis = 1 specifies to appply on the column
 
-q_movies = q_movies.sort_values('score', ascending=False)
+top_movies = top_movies.sort_values('score', ascending=False)
+#ascending false ensures it goes from best to worst if u go from top to bottom
 
+#top 15 movies based on weigthed rating system
+print(top_movies[['title', 'vote_count', 'vote_average', 'score']].head(20))
 
-# Create a simple user-item matrix
-data = {'User1': [4, 0, 3, 5],
-        'User2': [5, 5, 4, 0],
-        'User3': [0, 4, 0, 3],
-        'User4': [3, 0, 0, 2]}
+############### MOVIES NOW RATED ON COMMON SCALE AND SORTED IN DESCENDING ORDER #########################################
 
-df = pd.DataFrame(data, index=['Item1', 'Item2', 'Item3', 'Item4'])
+'''
+SO far we have a weighted rating system with the movies sorted in descending order (best to worst) and that score is like a new column
 
-# Transpose the matrix so that items are in the columns and users are in the rows
-df_T = df.T
-# Compute the cosine similarity
-cosine_sim = cosine_similarity(df_T)
-cosine_sim_df = pd.DataFrame(cosine_sim, index=df_T.columns, columns=df_T.columns)
-print(cosine_sim_df)
+Next Steps:
+Ensure language is compatible with user? or ignore for now and assume theyre ok w subtitles
 
 
+Find similar movies to that which the user inputted and make a subset of them
+How? 
+- BoW model comparing the frequency of related words in keywords/overview column 
+- Within the new subset sort the movies based how many genre tags they have in common from most to least
+ or something like: from the movies with more than x genre tags in common to the input select the 5 highest rated ones
+
+- to convert to the new model i will create a new column/row to append like we did with the new weighted score
+   to the new subset with  .apply which can take weighted rating as a parameter 
+   *still figuring out syntax for that and need ot make sure i fully get what ali meant
+
+- the TF-IDF is: Term Frequency-Inverse Document Frequency which sounds to me like BoW
+if so i will use library to import a module which can help
+
+- for cosine similarity we should know what we are comparing:
+ genres in common,
+ keywords in common, bow
+ overview similarity (bow)
+ actors/directors,
+ i thibnk all can be done wuth tf-idf. Chatgpt also said using tf-idf is the best way to convert genres into numbers
+
+
+
+'''
+
+#                  Genres          Lang.           keywords? 
+users = {'User1': [[10,2,5,....], [45,5,0,0,0...], [39,8,9,2,5, 7,11,...]]}
+
+
+
+# create this new dataframe by appending our binary genre checker format      using .apply() to create a new column just like the weigthed rating 
+          #             genres         language       keywords       metadata
+new_subset = top_movies[[1,0,1,0,0,1], [1,0,0,0...] , [1,2,3.,0,0], ['title', 'vote_count', 'vote_average', 'score],]
+
+
+
+
+distance of genres: ai = (100000 - number of genre for user)  e.g. action:26 representing 26 action movies inputted
+ bi = number of genre for the movie ( 0 or 1)  -> cosine similarity
 
 
 
@@ -65,20 +99,34 @@ print(cosine_sim_df)
 
 
 
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        n = 1
-        i = 0
-        for num in nums:
-            if nums[i] + nums[n] == target:
-                return i, n
-            
-            else:
-                n+=1
-                
-            i+=1
+
+
+
+
+
+
+
+
+
+
+
+# # Create a simple user-item matrix
+# data = {'User1': [4, 0, 3, 5],
+#         'User2': [5, 5, 4, 0],
+#         'User3': [0, 4, 0, 3],
+#         'User4': [3, 0, 0, 2]}
+
+# df = pd.DataFrame(data, index=['Item1', 'Item2', 'Item3', 'Item4'])
+
+# # Transpose the matrix so that items are in the columns and users are in the rows
+# df_T = df.T
+# # Compute the cosine similarity
+# cosine_sim = cosine_similarity(df_T)
+# cosine_sim_df = pd.DataFrame(cosine_sim, index=df_T.columns, columns=df_T.columns)
+# print(cosine_sim_df)
+
+
+
+
+
+
